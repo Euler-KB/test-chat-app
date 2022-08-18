fd594d2c262beaee0cf71f6546bb7bb62354a8ac
Kofi Boahene <euler-kb@live.com>
2022-08-18 00:09:47 +0000

    feat: define action types

---
+++ src/redux/actionTypes/index.js
@@ -0,0 +1,5 @@
+export const SEND_MESSAGE = "SEND_MESSAGE";
+export const UPDATE_MESSAGE = "UPDATE_MESSAGE";
+export const UPDATE_USERS = "UPDATE_USERS";
+export const ADD_USER = "ADD_USER";
+export const LOGOUT = "LOGOUT_USER";
